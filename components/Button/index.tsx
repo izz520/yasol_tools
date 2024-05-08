910d3d82e8083447f86184430677ae929db769c7
izz520 <1547471276@qq.com>
2024-05-08 15:04:16 +0800

    fix: import sort bug

--- components/Button/index.tsx
+++ components/Button/index.tsx
@@ -1,4 +1,5 @@
 import React, { PropsWithChildren } from 'react'
+
 import { twMerge } from 'tailwind-merge'
 const Button = ({ children }: PropsWithChildren) => {
   return <div className={twMerge('rounded bg-blue-500 px-4 py-2 font-bold text-white')}>{children}</div>
