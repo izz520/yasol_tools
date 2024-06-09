57f578327b8a5d4db382b856aa774bbcb86be8ef
izz520 <1547471276@qq.com>
2024-06-09 16:47:21 +0800

    feat: .vscode add prettier config path

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Home() {
   }
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen  flex-col items-center justify-between p-24">
       <Button onClick={updateCount}>{count}</Button>
       <Input />
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
